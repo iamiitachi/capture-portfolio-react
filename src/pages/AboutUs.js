1aea69b76f568dd974361301157c0e1211560a45
casimir101 <sanketnit0@gmail.com>
2021-02-03 16:13:54 +0530

    Add AboutUs

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AboutSection from '../components/AboutSection';
+
+const AboutUs = () => {
+  return (
+    <AboutSection />
+    // Faq
+  );
+};
+
+export default AboutUs;
