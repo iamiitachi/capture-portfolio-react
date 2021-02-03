de69f9f4ce9b6fa5a74088a2c241fe1ee855090a
casimir101 <sanketnit0@gmail.com>
2021-02-03 19:42:18 +0530

    Add Styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 // Importing pages
 import AboutUs from './pages/AboutUs';
+import GlobalStyle from './components/GlobalStyle';
 
 function App() {
   return (
     <div className="App">
       <AboutUs />
+      <GlobalStyle />
     </div>
   );
 }
